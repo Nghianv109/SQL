7c67cd31640e6c4e904fcd3a28f78a4f67973f87
Nguyen Van Cuong <nguyenvannghia.ftu2@gmail.com>
2022-01-16 15:39:58 +0700

    Tạo file SQL challange

--- SQL_Challange_HackerRank.sql
+++ SQL_Challange_HackerRank.sql
@@ -58,13 +58,15 @@ JOIN Challenges AS cha
 ON cha.college_id = col.college_id
 
 LEFT JOIN 
-    (SELECT ss.challenge_id, SUM(ss.total_submissions) AS total_submissions,                    SUM(ss.total_accepted_submissions) AS total_accepted_submissions 
+    (SELECT ss.challenge_id, SUM(ss.total_submissions) AS total_submissions,
+    SUM(ss.total_accepted_submissions) AS total_accepted_submissions 
      FROM Submission_Stats AS ss 
      GROUP BY ss.challenge_id) AS sg
 ON cha.challenge_id = sg.challenge_id
 
 LEFT JOIN
-    (SELECT vs.challenge_id, SUM(vs.total_views) AS total_views,                                SUM(vs.total_unique_views) AS total_unique_views
+    (SELECT vs.challenge_id, SUM(vs.total_views) AS total_views,                                
+    SUM(vs.total_unique_views) AS total_unique_views
      FROM View_Stats AS vs 
      GROUP BY vs.challenge_id) AS vg
 ON cha.challenge_id = vg.challenge_id
